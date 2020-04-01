35cf5e4b7498bc418de1f4007b38b87044bcd254
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-01 15:45:34 -0400

    Added new sprite to game board

--- main.ts
+++ main.ts
@@ -20,3 +20,24 @@ controller.moveSprite(mySprite, 100, 100)
 scene.setBackgroundColor(0)
 mySprite.say("Hello")
 mySprite.startEffect(effects.fire)
+let helloKitty = sprites.create(img`
+. . . . . . . . . . . . . 2 2 . . . . . . . . . 
+. . . . f f f . . . . . 2 3 3 2 . f f f . . . . 
+. . . f 1 1 1 f f f f 2 3 3 3 3 2 1 1 1 f . . . 
+. . . f 1 1 1 1 1 1 1 2 3 3 3 2 2 2 1 1 f . . . 
+. . . f 1 1 1 1 1 1 1 2 3 3 2 3 3 3 2 2 2 . . . 
+. . . . f 1 1 1 1 1 1 1 2 2 2 3 3 3 2 3 3 2 . . 
+. . . f 1 1 1 1 1 1 1 1 1 1 1 2 2 2 3 3 3 2 . . 
+. . . f 1 1 1 1 1 1 1 1 1 1 1 1 1 2 3 3 2 . . . 
+. . f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 . . . . 
+f f f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f f f f 
+. . f 1 1 1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 1 f . . 
+. f f f 1 1 1 f 1 1 1 1 1 1 1 1 f 1 1 1 f f f . 
+. . . f f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f f . . . 
+. . f f 1 1 1 1 1 1 1 f f 1 1 1 1 1 1 1 f f . . 
+. . . . f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f . . . . 
+. . . . . f f 1 1 1 1 1 1 1 1 1 1 f f . . . . . 
+. . . . . . . f f f f f f f f f f . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+helloKitty.setPosition(20, 20)
