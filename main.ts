d93529d27ad78933dca830cebf1ad7824dc57c30
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-01 23:36:44 -0400

    new sprite actions & dialogue

--- main.ts
+++ main.ts
@@ -2,17 +2,17 @@ color.setColor(3, color.rgb(233, 29, 99))
 let mySprite = sprites.create(img`
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-3 3 . . . . . . . . 3 . . . 3 3 
-3 3 . . . . . . . 3 3 3 . . 3 3 
-3 3 . . . d d d d d 3 3 3 . 3 3 
-3 3 . . d d d d d d d 3 3 3 3 3 
-3 3 . . d d d d d d d d 3 . 3 3 
-3 3 . . d d f d d d f d d . 3 3 
-3 3 . . d d d d d d d d d . 3 3 
-3 3 . . d d d d d d d d d . 3 3 
-3 3 . . d f f d d d f f d . 3 3 
-3 3 . . d d f f f f f d d . 3 3 
-3 3 . . . d d d d d d d . . 3 3 
+3 3 . . . . . . . 3 . . . . 3 3 
+3 3 . . . . . . 3 3 3 . . . 3 3 
+3 3 . . d d d d d 3 3 3 . . 3 3 
+3 3 . d d d d d d d 3 3 3 . 3 3 
+3 3 . d d d d d d d d 3 . . 3 3 
+3 3 . d d f d d d f d d . . 3 3 
+3 3 . d d d d d d d d d . . 3 3 
+3 3 . d d d d d d d d d . . 3 3 
+3 3 . d f f d d d f f d . . 3 3 
+3 3 . d d f f f f f d d . . 3 3 
+3 3 . . d d d d d d d . . . 3 3 
 3 3 . . . . . . . . . . . . 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
@@ -43,11 +43,18 @@ pause(2000)
 mySprite2.say("Hello", 2000)
 pause(2000)
 mySprite.startEffect(effects.fire)
+mySprite.vx += 40
+mySprite.vy += 60
+mySprite.setFlag(SpriteFlag.BounceOnWall, true)
 pause(2000)
 mySprite2.say("Whoa", 2000)
 pause(2000)
 mySprite2.say("You're on fire", 2000)
 pause(2000)
+mySprite2.startEffect(effects.halo)
+mySprite2.follow(mySprite, 50)
 mySprite.say("NO...", 2000)
 pause(2000)
 mySprite.say("YOU'RE ON FIRE", 5000)
+pause(5000)
+mySprite2.say("WHEE!!!")
